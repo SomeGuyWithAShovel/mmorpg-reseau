76097dd6e1cd276ac946479fbbe7b6ee65221b15
Matéu Mirade <mateu@pellaptop.org>
2026-06-14 19:27:44 +0200

    Changed pub_sub main to use shared

--- pub_sub/src/main.rs
+++ pub_sub/src/main.rs
@@ -1,5 +1,5 @@
 use bytes::Bytes;
-use shared::game_message::{GameMessage, PeerType};
+use shared::game_message::{GameMessage, PeerType, ClientId, Topic};
 
 #[allow(unused)]
 use log::{debug, info, warn, error};
@@ -62,41 +62,25 @@ fn test_pub_sub_process_packets()
     
     // Since we don't pass the peer_socket_id directly in the function we call, we need to register them first.
 
+    let client_id = 0x0102030405060708090a0b0c0d0e0f10;
+    
     pub_sub.process_received_packet(
         123_u128,
-        Bytes::from_static(
-            // register(u8), peer_type(u8), peer_id(u32)
-            &[GameMessage::REGISTER,/**/ PeerType::CLIENT,/**/
-              0x01, 0x02, 0x03, 0x04,
-              0x05, 0x06, 0x03, 0x04,
-              0x09, 0x0a, 0x0b, 0x0c,
-              0x0d, 0x0e, 0x0f, 0x10,
-            ]
-    ));
+        GameMessage::Register {
+            client_id: ClientId::of_player(client_id)
+        }.as_bytes());
 
     pub_sub.process_received_packet(
         456_u128,
-        Bytes::from_static(
-            // register(u8), peer_type(u8), peer_id(u32)
-            &[GameMessage::REGISTER,/**/ PeerType::GAME_SERVER,/**/
-              0x01, 0x02, 0x03, 0x04,
-              0x05, 0x06, 0x03, 0x04,
-              0x09, 0x0a, 0x0b, 0x0c,
-              0x0d, 0x0e, 0x0f, 0x10,
-            ]
-    ));
+        GameMessage::Register {
+            client_id: ClientId::of_game_server(client_id)
+        }.as_bytes());
 
     pub_sub.process_received_packet(
         789_u128,
-        Bytes::from_static(
-            // register(u8), peer_type(u8), peer_id(u32)
-            &[GameMessage::REGISTER,/**/ PeerType::OTHER_SERVER,/**/
-              0x01, 0x02, 0x03, 0x04,
-              0x05, 0x06, 0x03, 0x04,
-              0x09, 0x0a, 0x0b, 0x0c,
-              0x0d, 0x0e, 0x0f, 0x10,
-            ]
-    ));
+        GameMessage::Register {
+            client_id: ClientId::of_other_server(client_id)
+        }.as_bytes());
 
     // all 3 have the same ID, so we can test if the Peer Types works as intended
     // register complete
@@ -104,34 +88,27 @@ fn test_pub_sub_process_packets()
 
     pub_sub.process_received_packet(
         0_u128, // doesn't matter who subscribes, what matters is who is being subscribed
-        Bytes::from_static(
-            // subscribe(u8), peer_type(u8), peer_id(u32), topic_size(u16), topic(&str)
-            &[GameMessage::SUBSCRIBE,/**/ PeerType::CLIENT,/**/
-              0x01, 0x02, 0x03, 0x04,
-              0x05, 0x06, 0x03, 0x04,
-              0x09, 0x0a, 0x0b, 0x0c,
-              0x0d, 0x0e, 0x0f, 0x10,/**/ 0x00, 0x03,/**/ b'a', b'a', b'a']
-    )); // CLIENT 0x01020304... = peer_socket_id 123 (being subscribed by 000 but we don't care by who it is subscribed)
+        GameMessage::Subscribe {
+            client_id: ClientId::of_player(client_id),
+            topic: Topic("aaa".to_string()),
+        }.as_bytes()
+    ); // CLIENT 0x01020304... = peer_socket_id 123 (being subscribed by 000 but we don't care by who it is subscribed)
 
     pub_sub.process_received_packet(
         0_u128,
-        Bytes::from_static(
-            &[GameMessage::SUBSCRIBE,/**/ PeerType::GAME_SERVER,/**/
-              0x01, 0x02, 0x03, 0x04,
-              0x05, 0x06, 0x03, 0x04,
-              0x09, 0x0a, 0x0b, 0x0c,
-              0x0d, 0x0e, 0x0f, 0x10,/**/ 0x00, 0x03,/**/ b'a', b'a', b'a']
-    )); // SERVER 0x01020304... = peer_socket_id 456
+        GameMessage::Subscribe {
+            client_id: ClientId::of_game_server(client_id),
+            topic: Topic("aaa".to_string()),
+        }.as_bytes()
+    ); // SERVER 0x01020304... = peer_socket_id 456
 
     pub_sub.process_received_packet(
         0_u128,
-        Bytes::from_static(
-            &[GameMessage::SUBSCRIBE,/**/ PeerType::OTHER_SERVER,/**/
-              0x01, 0x02, 0x03, 0x04,
-              0x05, 0x06, 0x03, 0x04,
-              0x09, 0x0a, 0x0b, 0x0c,
-              0x0d, 0x0e, 0x0f, 0x10,/**/ 0x00, 0x03,/**/ b'b', b'b', b'b']
-    )); // OTHER 0x01020304 = peer_socket_id 789
+        GameMessage::Subscribe {
+            client_id: ClientId::of_other_server(client_id),
+            topic: Topic("bbb".to_string()),
+        }.as_bytes()
+    ); // OTHER 0x01020304 = peer_socket_id 789
 
     // subscribes completed
     info!("topic_subs: {:?}\n", pub_sub.topic_subs);
@@ -139,62 +116,71 @@ fn test_pub_sub_process_packets()
 
     pub_sub.process_received_packet(
         0_u128, // doesn't matter who publishes
-        Bytes::from_static(
-            // publish(u8), topic_size(u16), topic(&str), data_size(u16), data(&[u8])
-            &[GameMessage::PUBLISH,/**/ 0x00, 0x03,/**/ b'a', b'a', b'a',/**/ 0x00, 0x03,/**/ 0x11, 0x22, 0x33]
-    ));
+        GameMessage::Publish {
+            topic: Topic("aaa".to_string()),
+            payload: vec![0x11, 0x22, 0x33],
+        }.as_bytes()
+    );
     PubSub::flush_peer_buffers(&mut pub_sub.subs_buffers);
 
     pub_sub.process_received_packet(
         0_u128,
-        Bytes::from_static(
-            &[GameMessage::PUBLISH,/**/ 0x00, 0x03,/**/ b'a', b'a', b'a',/**/ 0x00, 0x03,/**/ 0x11, 0x22, 0x33] // same data as previous publish
-    ));
+        // same data as previous publish
+        GameMessage::Publish {
+            topic: Topic("aaa".to_string()),
+            payload: vec![0x11, 0x22, 0x33],
+        }.as_bytes()
+    );
     PubSub::flush_peer_buffers(&mut pub_sub.subs_buffers);
 
     pub_sub.process_received_packet(
         0_u128,
-        Bytes::from_static(
-            &[GameMessage::PUBLISH,/**/ 0x00, 0x03,/**/ b'a', b'a', b'a',/**/ 0x00, 0x04,/**/ 0x11, 0x22, 0x33, 0x44] // data is longer
-    ));
+        // data is longer
+        GameMessage::Publish {
+            topic: Topic("aaa".to_string()),
+            payload: vec![0x11, 0x22, 0x33, 0x44],
+        }.as_bytes()
+    );
     PubSub::flush_peer_buffers(&mut pub_sub.subs_buffers);
 
     pub_sub.process_received_packet(
         0_u128,
-        Bytes::from_static(
-            &[GameMessage::PUBLISH,/**/ 0x00, 0x03,/**/ b'b', b'b', b'b',/**/ 0x00, 0x03,/**/ 0x11, 0x22, 0x33] // topic is different
-    ));
+        // topic is different
+        GameMessage::Publish {
+            topic: Topic("bbb".to_string()),
+            payload: vec![0x11, 0x22, 0x33],
+        }.as_bytes()
+    );
     PubSub::flush_peer_buffers(&mut pub_sub.subs_buffers);
 
     // ---------------------------------------------------------------------------------------------------------------
 
     pub_sub.process_received_packet(
         0_u128,
-        Bytes::from_static(
-            // unsubscribe(u8), peer_type(u8), peer_id(u32), topic_size(u16), topic(&str)
-            &[GameMessage::UNSUBSCRIBE,/**/ PeerType::GAME_SERVER,/**/
-              0x01, 0x02, 0x03, 0x04,
-              0x05, 0x06, 0x03, 0x04,
-              0x09, 0x0a, 0x0b, 0x0c,
-              0x0d, 0x0e, 0x0f, 0x10,/**/ 0x00, 0x03,/**/ b'a', b'a', b'a']
-    ));
+        GameMessage::Unsubscribe {
+            client_id: ClientId::of_game_server(client_id),
+            topic: Topic("aaa".to_string()),
+        }.as_bytes()
+    );
     
     info!("topic_subs: {:?}\n", pub_sub.topic_subs);
     
     pub_sub.process_received_packet(
         0_u128,
-        Bytes::from_static(
-            // publish(u8), topic_size(u16), topic(&str), data_size(u16), data(&[u8])
-            &[GameMessage::PUBLISH,/**/ 0x00, 0x03,/**/ b'a', b'a', b'a',/**/ 0x00, 0x03,/**/ 0x11, 0x22, 0x33]
-    ));
+        GameMessage::Publish {
+            topic: Topic("aaa".to_string()),
+            payload: vec![0x11, 0x22, 0x33],
+        }.as_bytes()
+    );
     PubSub::flush_peer_buffers(&mut pub_sub.subs_buffers);
 
     pub_sub.process_received_packet(
         0_u128,
-        Bytes::from_static(
-            // publish(u8), topic_size(u16), topic(&str), data_size(u16), data(&[u8])
-            &[GameMessage::PUBLISH,/**/ 0x00, 0x03,/**/ b'x', b'y', b'z',/**/ 0x00, 0x05,/**/ 0x12, 0x34, 0x56, 0x78, 0x90]
-    ));
+        GameMessage::Publish {
+            topic: Topic("xyz".to_string()),
+            payload: vec![0x12, 0x34, 0x56, 0x78, 0x90],
+        }.as_bytes()
+    );
     PubSub::flush_peer_buffers(&mut pub_sub.subs_buffers);
 
     
